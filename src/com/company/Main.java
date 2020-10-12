cc50761b58cf320e4e4448e8193aedcaaf52b907
Alexander Rogov <71908608+mlechnyput@users.noreply.github.com>
2020-10-12 13:06:20 +0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,8 +9,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter the string :");
-        String str = scan.nextLine();
-        meth(str);
+        meth(scan.nextLine());
     }
 
     private static void meth(String str) {
